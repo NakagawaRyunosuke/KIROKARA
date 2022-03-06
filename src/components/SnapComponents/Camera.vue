1f0ef77c338910493780f09ed7daff3b72b8356b
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-03-07 01:23:42 +0900

    edit

--- src/components/SnapComponents/Camera.vue
+++ src/components/SnapComponents/Camera.vue
@@ -51,29 +51,7 @@ export default {
             this.videoHidden = "hidden";
             this.canvasHidde = "";
         },
-        textCheck(){
-            const APIKEY = process.env.VUE_APP_APIKEY;
-            const ENDPOINT = process.env.VUE_APP_ENDPOINT;
-            const imgURL = this.makeblob(this.captures[this.captures.length - 1]);
-        },
-        makeblob(dataURL){
-            let BASE64_MARKER = ';base64,';
-            if (dataURL.indexOf(BASE64_MARKER) == -1) {
-                let parts = dataURL.split(',');
-                let contentType = parts[0].split(':')[1];
-                let raw = decodeURIComponent(parts[1]);
-                return new Blob([raw], {type: contentType});
-            }
-            let parts = dataURL.split(BASE64_MARKER);
-            let contentType = parts[0].split(':')[1];
-            let raw = window.atob(parts[1]);
-            let rawLength = raw.length;
-            let uInt8Array = new Uint8Array(rawLength);
-            for (let i = 0; i < rawLength; ++i) {
-                uInt8Array[i] = raw.charCodeAt(i);
-            }
-            return new Blob([uInt8Array], {type: contentType})
-        },
+        
     }
 }
 </script>
